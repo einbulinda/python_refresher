b1187a34ed752118ac2690be679ff590362f95f3
Einstein Bulinda <einbulinda@gmail.com>
2024-03-09 08:28:13 +0300

    lists, tuples and sets introduction

--- main.py
+++ main.py
@@ -1,42 +1,78 @@
+
+# Lists Tuples & Sets
+
+courses = ['History', 'Physics', 'Math', 'CompSci']
+# courses_2 = ['Art', 'Education']
+nums = [1, 5, 8, 6, 9]
+
+# courses.append(courses_2)  # Will add the actual list as a member @ end.
+
+# courses.insert(2, "Mathematics") # Takes 2 values, (index, item to add)
+
+# courses.extend(courses_2) # adds individual courses 2 values.
+
+# Removing values
+
+# courses.remove('Math')
+# popped = courses.pop()
+
+# courses.reverse()
+# courses.sort(reverse=True)
+sorted_courses = sorted(courses)  # Sorted version of list. No altering of original.
+
+# Find index of CompSci
+# print(courses.index('CompSci'))
+
+# Check if item is in list
+# print('Art' in courses)
+
+'''
+for index, course in enumerate(courses, start=1):
+    print(index, course)
+'''
+# Turn list in comma separated string
+course_str = ', '.join(courses)
+
+# String into list
+new_list = course_str.split(', ')
+
+
 """
-if it's hot
-    It's a hot day
-    Drink plenty of water
-otherwise if it's cold
-    It's a cold day
-    Wear warm clothes
-otherwise
-    It's a lovely day.
+TUPLES
+lists are mutable, tuples are immutable
+tuples are used for list of values that you dont intent to change
+We use brackets instead of square brackets
+No appending, removing, adding to tuples
 """
-'''
-is_hot = False
-is_cold = True
 
-if is_hot:
-    print("It's a hot day")
-    print("Drink plenty water")
-elif is_cold:
-    print("It's a cold day")
-    print("Wear warm clothes")
-else:
-    print("It's a lovely day")
-print("Enjoy your day.")
+"""
+SETS
+Unordered values
+orders change on execution
+They remove duplicates on execution
+created with curly brackets
+They are optimized for membership tests
+intersection method - item in both sets
+difference method - not in both
+union method - combine sets.
 
-'''
+"""
 
-# Exercise
-'''
-Price of a house is 1M
-If a buyer has good credit,
-    they need to put down 10%
-Otherwise
-    They need to put down 20%
-Print the down payment'''
+cs_courses = {'History', 'Math', 'Physics', 'Math'}
 
-has_good_credit = False
-price = 1_000_000
+print('Math' in cs_courses)  # membership test
 
-if has_good_credit:
-    print(f"Down payment: Ksh. {price * 0.1}")
-else:
-    print(f"Down payment: Ksh. {price * 0.2}")
+# Creating empty lists, sets and tuples
+
+# Empty List
+empty_list = []
+empty_list_2 = list()
+
+# Empty Tuple
+empty_tuple = ()
+empty_tuple_2 = tuple()
+
+# Empty Set
+empty_set = set()
+not_empty_set = {}  # This will create a dict
+
