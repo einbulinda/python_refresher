5e7a4cc6bb49ff47c8609aecd72229bd6e2097c1
Einstein Bulinda <einbulinda@gmail.com>
2024-03-16 08:12:34 +0300

    functions

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
-nums = [1, 2, 3, 4, 5]
+# FUNCTIONS
 
-'''
-for num in nums:
-    if num == 3:
-        print('Found 3')
-        break
-    print(num)
-'''
-"""for num in nums:
-    if num == 3:
-        print('Found 3')
-        continue
-    print(num)"""
 
-# Break & Continue
+"""
+def hello_func():
+    pass  # We don't want to pass in things now and want to leave it blank.
 
-# Loop within a Loop
+print(hello_func())
+"""
 
-'''for num in nums:
-    for letter in 'abc':
-        print(num, letter)'''
 
-# Avoid nested loops
-# Another way to loop is by range kw
+def hello_func(greeting, name='You'):
+    return '{}, {}.'.format(greeting, name)
 
-'''for i in range(1, 10):
-    print(i)'''
 
-# While loop - runs until condition evaluates to false
+# print(hello_func('Hi', 'Corey'))
 
-x = 0
-'''while x < 10:
-    if x == 5:
-        break
-    print(x)
-    x += 1'''
+# POSITIONAL KEYWORD ARGUMENTS
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
 
-# indefinite loop until value is found
-while True:
-    if x == 5:
-        break
-    print(x)
-    x += 1
 
+courses = ['Math', 'Art']
+info = {'name': 'John', 'age': 22}
+
+# student_info('Math', 'Art', name='John', age=22)
+
+# student_info(courses, info)
+
+# student_info(*courses, **info)
+
+# EXAMPLES OF FUNCTIONS
+
+# Number of days per month, first value placeholder for indexing purposes.
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap(year):
+    """Returns True for leap years and False for non-leap years"""  # Doc string helps define what a function does.
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    """Returns number of days in that month in that year"""
+
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+
+print(days_in_month(2025, 2))
