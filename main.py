462861bb3d41e3baa36a94233d1874a8604c675d
Einstein Bulinda <einbulinda@gmail.com>
2024-03-16 08:36:24 +0300

    modules

--- main.py
+++ main.py
@@ -1,56 +1,17 @@
-# FUNCTIONS
+import datetime
+import calendar
+
+from my_modules import find_index, test  # gives access to the function only
+import sys
+
+# import my_modules as mn # importing with alias name.
 
 
-"""
-def hello_func():
-    pass  # We don't want to pass in things now and want to leave it blank.
+courses = ['History', 'Math', 'Physics', 'CompSci']
 
-print(hello_func())
-"""
+index = find_index(courses, 'Math')
+# print(index)
+# print(test)
 
-
-def hello_func(greeting, name='You'):
-    return '{}, {}.'.format(greeting, name)
-
-
-# print(hello_func('Hi', 'Corey'))
-
-# POSITIONAL KEYWORD ARGUMENTS
-def student_info(*args, **kwargs):
-    print(args)
-    print(kwargs)
-
-
-courses = ['Math', 'Art']
-info = {'name': 'John', 'age': 22}
-
-# student_info('Math', 'Art', name='John', age=22)
-
-# student_info(courses, info)
-
-# student_info(*courses, **info)
-
-# EXAMPLES OF FUNCTIONS
-
-# Number of days per month, first value placeholder for indexing purposes.
-month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-
-def is_leap(year):
-    """Returns True for leap years and False for non-leap years"""  # Doc string helps define what a function does.
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
-
-def days_in_month(year, month):
-    """Returns number of days in that month in that year"""
-
-    if not 1 <= month <= 12:
-        return 'Invalid Month'
-
-    if month == 2 and is_leap(year):
-        return 29
-
-    return month_days[month]
-
-
-print(days_in_month(2025, 2))
+print(datetime.date.today())
+print(calendar.isleap(2009))
---
+++ my_modules.py
@@ -0,0 +1,13 @@
+print('Imported my_module...')
+
+test = 'Test String'
+
+
+def find_index(to_search,target):
+    """Find index of a value in a sequence"""
+    for i, value in enumerate(to_search):
+        if value == target:
+            return 1
+
+    return -1
+
